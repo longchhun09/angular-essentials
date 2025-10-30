b2fefa69688829b21ce82789989011f7b4122bcb
longchhun <mark.chhunlong@gmail.com>
2025-10-30 11:48:53 +0700

    Refactor user component to use signals and computed properties: update selectedUser handling and image path logic in TypeScript and HTML for improved reactivity.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '@app/dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,13 +11,10 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 })
 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }
+  protected selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
-    console.log('click user')
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
