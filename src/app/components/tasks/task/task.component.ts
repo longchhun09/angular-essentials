242fc5c2b9302cf8e24839e154a4057577eb1085
longchhun <mark.chhunlong@gmail.com>
2025-10-31 11:35:49 +0700

    Add task component and integrate it into tasks display: implement task rendering logic, create task component with HTML and SCSS, and update tasks component to manage task data.

---
+++ src/app/components/tasks/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss',
+})
+export class TaskComponent {
+  @Input({required: true}) task!: any;
+}
--- src/app/components/tasks/tasks.ts
+++ src/app/components/tasks/tasks.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss',
 })
 export class Tasks {
   @Input({required: true}) name!: string;
 
+  protected readonly tasks = [
+    { id: 't1', userId: 'u1', title: 'Task 1', summary: 'Task 1 summary', dueDate: '2025-10-31' },
+    { id: 't2', userId: 'u2', title: 'Task 2', summary: 'Task 2 summary', dueDate: '2025-02-18' },
+    { id: 't3', userId: 'u3', title: 'Task 3', summary: 'Task 3 summary', dueDate: '2025-09-23' },
+  ];
+
+  get task() {
+    return this.tasks[0];
+  }
 }
